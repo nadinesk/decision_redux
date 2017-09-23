b4fa927a871ee384fc4ff505c8b2613c7e46301f
nadinesk <nadine.khattak@gmail.com>
2017-09-23 07:41:25 -0400

    added sum

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,26 +1,84 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import Item from './Item'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
-const ItemList = ({ items }) => (
 
-	<div> 
-	<ul>  
-		{items.map(item => 
+
+
+class ItemList extends Component {
+
+	constructor(props) {
+		super(props) 
+
+	}
+
+	makeSum() {
+		
+		var proItems = this.props.items.filter(function(item) {
+			return item.category == 'pro'
+		})
+		
+		var proWeights = proItems.map(item => item.weight)		
+
+		var proWeightsNum = proWeights.map(function(weight) {
+			return parseFloat(weight)
+		})
+
+		var proWeightsSum =   proWeightsNum.reduce(function(a,b) { 		
+			return a + b;}, 0)
+
+		var conItems = this.props.items.filter(function(item) {
+			return item.category == 'con'
+		})
+		
+		var conWeights = conItems.map(item => item.weight)		
+
+		var conWeightsNum = conWeights.map(function(weight) {
+			return parseFloat(weight)
+		})
+
+		var conWeightsSum =   conWeightsNum.reduce(function(a,b) { 		
+			return a + b;}, 0)
+		
+		return proWeightsSum - conWeightsSum
+
+
+	    }
+
+
+
+	render() {
+
+		
+		
+
+
+		
+
+		return (
+			<div> 
+				<ul>  
+					{this.props.items.map(item => 
+						
+							<Item key={item.id}  {...item} /> 
+					)}
+
+				</ul> 
 			
-				<Item key={item.id}  {...item} /> 
-			
 
-		)}
+				
+			<p>{this.makeSum()}</p>
 
-	</ul> 
+			</div> 
 
-	
 
-	
-	</div> 
+		)
+	}
 
-)
+}
+
 
 ItemList.propTypes = {
 	items: PropTypes.arrayOf(
@@ -31,5 +89,14 @@ ItemList.propTypes = {
 		})
 	).isRequired, 
 }
+
+
+
+function mapStateToProps(state){
+  return { items: state.items}
+}
+
+ItemList = connect(mapStateToProps, null)(ItemList)
+
 
 export default ItemList
